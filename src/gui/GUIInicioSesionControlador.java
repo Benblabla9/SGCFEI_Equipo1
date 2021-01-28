c9d3f7bd3d2971ee8e30b9bef70902425516d6a3
Ivana Correa <luzita_ivanita@hotmail.com>
2021-01-28 17:21:00 -0600

    Avances de login

--- src/gui/GUIInicioSesionControlador.java
+++ src/gui/GUIInicioSesionControlador.java
@@ -1,19 +1,71 @@
 package gui;
 
+import accesoDatos.controladoresDAO.UsuarioDAOImpl;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import logica.ValidacionGeneral;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GUIInicioSesionControlador extends GUIControladorGeneral implements Initializable {
-    @FXML private Button btnKillme;
+    @FXML private Button btnIngresar;
+    @FXML private TextField tftNombreUsuario;
+    @FXML private TextField tftContrasenia;
 
     @Override
-    public void initialize(URL url, ResourceBundle rb) { }
+    public void initialize(URL url, ResourceBundle rb) {
+        limiteTextField(tftNombreUsuario,20);
+        limiteTextField(tftContrasenia, 20);
+    }
 
-    public void killMe() {
+    public void ingresar() {
+        boolean esValidoInformacion = validateInformation();
+        if(esValidoInformacion) {
+            String nombreUsuario = tftNombreUsuario.getText();
+            String contrasenia = tftContrasenia.getText();
+            UsuarioDAOImpl cuentaDAO = new UsuarioDAOImpl();
+            boolean esCuenta = cuentaDAO.buscarUsuario(nombreUsuario,contrasenia);
+            if(esCuenta == false){
+                generarError("La cuenta no existe. Registrese");
+            }else {
+                if(esCuenta == true){
+                    String tipo = cuentaDAO.rolUsuario(nombreUsuario,contrasenia);
+                    switch (tipo) {
+                        case "Docente":
+                            abrirVentana("/gui/docente/GUIMenuDocente.fxml", btnIngresar);
+                            break;
+                        case "Coordinador":
+                            abrirVentana("/gui/coordinador/GUIMenuCoordinador.fxml", btnIngresar);
+                            break;
+                        case "Administrador":
+                            abrirVentana("/gui/administrador/GUIMenuAdministrador.fxml", btnIngresar);
+                            break;
+                        default: generarInformation(tipo);
+                    }
+                }else {
+                    generarError("Error en la conexion. Intente más tarde");
+                }
+            }
+        }else {
+            generarAlerta("Ingresar datos válidos");
+        }
+    }
 
+    private boolean validateInformation() {
+        boolean validacion = true;
+        ValidacionGeneral validacionGeneral = new ValidacionGeneral();
+        boolean esValidoCuenta = validacionGeneral.validarPalabra(tftNombreUsuario.getText());
+        if (!esValidoCuenta) {
+            tftNombreUsuario.getStyleClass().add("error");
+            validacion = false;
+        }
+        boolean esValidoContrasenia = validacionGeneral.validarPalabra(tftContrasenia.getText());
+        if (!esValidoContrasenia) {
+            validacion = false;
+        }
+        return  validacion;
     }
 }
 
---
+++ src/logica/ValidacionGeneral.java
@@ -0,0 +1,162 @@
+package logica;
+
+import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidacionGeneral {
+    public String eliminarEspacios (String palabras) {
+        if(palabras.length()>0) {
+            palabras = palabras.replaceAll("\\s+", " ");
+            if (palabras.charAt(palabras.length() - 1) == ' ') {
+                palabras = palabras.substring(0, palabras.length() - 1);
+            }
+        }
+        return palabras;
+    }
+
+    public boolean validarPalabra (String palabras) {
+        boolean esValido;
+        if(palabras.length()!= 0){
+            esValido=true;
+        }else{
+            esValido=false;
+        }
+        return esValido;
+    }
+
+    public boolean validarCorreo (String correo) {
+        boolean esValidoCorreo;
+        Pattern pattern = Pattern
+                .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Matcher mather = pattern.matcher(correo);
+        esValidoCorreo = mather.find();
+        return esValidoCorreo;
+    }
+
+    public boolean validarNombre (String nombre) {
+        boolean esValidoNombre;
+        Pattern pattern = Pattern
+                .compile("[A-Za-z]{3,50}");
+        Matcher mather = pattern.matcher(nombre);
+        esValidoNombre = mather.find();
+        return esValidoNombre;
+    }
+
+    public boolean validarTelefono (String telefono) {
+        boolean esValidoTelefono;
+        Pattern pattern = Pattern
+                .compile("[0-9]{10}");
+        Matcher mather = pattern.matcher(telefono);
+        esValidoTelefono = mather.find();
+        return esValidoTelefono;
+    }
+
+    public boolean validarRNC (String nrc) {
+        boolean esValidoNRC;
+        Pattern pattern = Pattern.compile("[0-9]{10}");
+        Matcher mather = pattern.matcher(nrc);
+        esValidoNRC = mather.find();
+        return esValidoNRC;
+    }
+
+    public boolean validarContrasenia (String contrasenia) {
+        Pattern pattern = Pattern
+                .compile("[A-Za-z0-9]{10,20}");
+        Matcher mather = pattern.matcher(contrasenia);
+        boolean esValidoContrasenia = mather.find();
+        return esValidoContrasenia;
+    }
+
+    public boolean validarApellidos (String apellidos) {
+        boolean esValidoApellidos;
+        Pattern patternName = Pattern
+                .compile("[A-Za-z]{3,50}");
+        Matcher mather = patternName.matcher(apellidos);
+        esValidoApellidos = mather.find();
+        return esValidoApellidos;
+    }
+
+    public boolean validarRFC (String rfc){
+        boolean isValidoRFC;
+        rfc=rfc.toUpperCase().trim();
+        isValidoRFC = rfc.toUpperCase().matches("^([A-ZÑ\\x26]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1]))([A-Z\\d]{3})?$");
+        if(isValidoRFC){
+            Calendar fechaActual = Calendar.getInstance();
+            int anioActual = fechaActual.get(Calendar.YEAR);
+            String anioActualCad = Integer. toString(anioActual);
+            String anioRFC = anioActualCad.charAt(2)+""+anioActualCad.charAt(3);
+            int anioRFCActual = Integer.parseInt(anioRFC);
+
+            String anio = rfc.charAt(4) +""+rfc.charAt(5);
+            int anioRFCIngresado = Integer.parseInt(anio);
+
+            int limiteAnio = anioActual-89;
+            String anioLimite = Integer. toString(limiteAnio);
+            String aniolimiteRFCCad = anioLimite.charAt(2)+""+anioLimite.charAt(3);
+            int anioLimiteRFC = Integer.parseInt(aniolimiteRFCCad);
+
+            if(anioRFCIngresado<anioRFCActual || anioRFCIngresado>anioLimiteRFC){
+                String mes = rfc.charAt(6) +""+rfc.charAt(7);
+                String dia = rfc.charAt(8) +""+rfc.charAt(9);
+                int diaRFC = Integer.parseInt(dia);
+                if((mes.equals("04")||mes.equals("06")||mes.equals("09")||mes.equals("11")) && diaRFC<31 ){
+                    isValidoRFC=true;
+                }else{
+                    if(mes.equals("02") && diaRFC<30){
+                        double promedioAnio = anioRFCIngresado%2;
+                        if(promedioAnio!=0 && diaRFC<=28){
+                            isValidoRFC=true;
+                        }else {
+                            if(promedioAnio==0 && diaRFC<=29){
+                                isValidoRFC=true;
+                            }else {
+                                isValidoRFC = false;
+                            }
+                        }
+                    }else {
+                        if(mes.equals("01")||mes.equals("03")||mes.equals("05")||mes.equals("07")||
+                                mes.equals("08")||mes.equals("10")||mes.equals("12")){
+                            isValidoRFC=true;
+                        }else {
+                            isValidoRFC = false;
+                        }
+                    }
+                }
+            }else {
+                isValidoRFC=false;
+            }
+        }
+        return isValidoRFC;
+    }
+
+    public boolean validarAño (String fecha) {
+        Calendar fechaActual = Calendar.getInstance();
+        boolean esValidoFecha=true;
+        String[] fechaNacimiento = fecha.split("/");
+        int anio = Integer.parseInt(fechaNacimiento[2]);
+        int anioActual = fechaActual.get(Calendar.YEAR);
+        int numeroAnios = anioActual-anio;
+        if(numeroAnios<18 || numeroAnios>89){
+            esValidoFecha=false;
+        }
+        return esValidoFecha;
+    }
+
+    public boolean validarConcidenciaRFC (String rfc, String fecha){
+        boolean esValidoConcidencia=true;
+        String[] fechaNacimiento = fecha.split("/");
+        String diaFecha = fechaNacimiento[0];
+        String mesFecha = fechaNacimiento[1];
+        String anioFecha = fechaNacimiento[2];
+        anioFecha = anioFecha.charAt(2)+""+anioFecha.charAt(3);
+        String anioRFC = rfc.charAt(4) +""+rfc.charAt(5);
+        String mesRFC = rfc.charAt(6) +""+rfc.charAt(7);
+        String diaRFC = rfc.charAt(8) +""+rfc.charAt(9);
+        if(!anioFecha.equals(anioRFC) || !mesFecha.equals(mesRFC) || !diaFecha.equals(diaRFC)){
+            esValidoConcidencia=false;
+        }
+        return  esValidoConcidencia;
+    }
+}
