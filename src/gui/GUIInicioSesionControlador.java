f491d8b0fe7971f11a0d64911e61bec441036bcc
Ivana Correa <luzita_ivanita@hotmail.com>
2021-01-28 15:39:47 -0600

    Avances proyecto

---
+++ src/gui/GUIInicioSesionControlador.java
@@ -0,0 +1,20 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GUIInicioSesionControlador extends GUIControladorGeneral implements Initializable {
+    @FXML private Button btnKillme;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) { }
+
+    public void killMe() {
+
+    }
+}
+
+
--- src/gui/main.java
+++ src/gui/main.java
@@ -1,4 +1,22 @@
 package gui;
 
-public class main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application {
+    @Override
+    public void start(Stage stageLogin) throws Exception {
+        Parent rootLogin = FXMLLoader.load(getClass().getResource("/gui/GUIInicioSesion.fxml"));
+        Scene sceneLogin = new Scene(rootLogin);
+        stageLogin.setScene(sceneLogin);
+        stageLogin.setResizable(false);
+        stageLogin.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
